b61f7a344df1e2c6ed85dc055889ec5ed624a35d
tagoremithun <tagoremithun@gmail.com>
2023-02-19 22:00:20 +0600

    function in javascript.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+console.log("hello world");
+
+function myFunc(){
+    
+    var a = 4;
+    var b= 5;
+    var sum =a+b;
+    window.alert(sum);
+}
+
+function currentDate(){
+    var currentDate; 
+    currentDate = Date();
+    alert(currentDate);
+
+}
